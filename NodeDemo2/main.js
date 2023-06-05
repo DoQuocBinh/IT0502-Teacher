37ceba26e75112d53600b021508290fe8a0ede96
DoQuocBinh <binhdq@fpt.edu.vn>
2023-06-05 16:06:44 +0700

    ok 3

--- NodeDemo2/main.js
+++ NodeDemo2/main.js
@@ -2,11 +2,14 @@ var express = require('express')
 const { rmSync } = require('fs')
 var app = express()
 
-app.get('/',(req,res)=>{
-    res.write("<html><body><h1 style='color:red'>Hello world</h1></body></html>")
-    res.end
-})
+app.use(express.static('public'))
 
+app.get('/',(req,res)=>{
+    res.sendFile(__dirname + "/danhsach.html")
+})
+app.get('/huyHoang',(req,res)=>{
+    res.sendFile(__dirname + "/sv_hoang.html")
+})
 app.get('/profile',(req,res)=>{
     console.log(__dirname)
     res.sendFile(__dirname +  '/profile.html')
