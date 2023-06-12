bed1929cffc9ebb0aaa2275cafdf160353def957
DoQuocBinh <binhdq@fpt.edu.vn>
2023-06-12 15:35:53 +0700

    find search

--- NodeDemo3/app.js
+++ NodeDemo3/app.js
@@ -7,16 +7,20 @@ const dsSV = [
         {id:1, name : "Linh", age : 20, phone : '0901223333'},
         {id:2, name : "Long", age : 23,phone : '0933533556'}
     ]
-
+'1'
 app.get('/',(req,res)=>{
     res.render('home',{danhSach : dsSV})
 })
 
-app.get('/student/:name',(req,res)=>{
-    let studentName = req.params.name
-    let tuoi = 20 //truy cap tu database
-    //res.sendFile(__dirname + "/"  + studentName + ".html")
-    res.render('student',{'name':studentName,'tuoi':tuoi})
+app.get('/student/:id',(req,res)=>{
+    let studentId = req.params.id
+    let student = dsSV.find(item => item.id ==studentId)
+    if(student){
+        res.render('student',{'student':student})
+    }else{
+        res.send('Not found!')
+    }
+    
 })
 
 
